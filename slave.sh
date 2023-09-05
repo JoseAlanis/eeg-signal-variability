47e5f5f40494b75f54ae64fd3f64010136d59c5d
Jose C. Garcia Alanis <12409129+JoseAlanis@users.noreply.github.com>
2023-09-05 13:06:08 +0200

    conda init

--- slave.sh
+++ slave.sh
@@ -6,9 +6,12 @@
 #SBATCH -N 1                 # number of tasks
 #SBATCH -t 00:02:00          # Run time (hh:mm:ss)
 
+CONDA_ENV = "/lustre/miifs01/project/m2_jgu-amd/josealanis/envs/mne-1.5"
+
 module purge
 module add lang/Anaconda3
-conda activate /lustre/miifs01/project/m2_jgu-amd/josealanis/envs/mne-1.5
+conda init bash
+conda activate $CONDA_ENV
 
 # Run Script
 srun python 201_compute_signal_variability_measures.py --subj $i --session 1 --task 'oddeven' --stimulus 'cue' --window 'post' --overwrite True --jobs 64
