0ee80ca9371885178b9ce2d1694546539bedf49e
José C. García Alanis <12409129+JoseAlanis@users.noreply.github.com>
2023-09-07 13:26:02 +0200

    add docstring, minor changes

--- signal_variability.py
+++ signal_variability.py
@@ -15,12 +15,31 @@ import antropy as ant
 import neurokit2 as nk
 
 
-def spectral_entropy(x):
-    # This part handles the case when the power spectrum density
-    # takes any zero value.
-    # It returns x * log(x) if x is positive, 0 if x == 0, and np.nan otherwise.
-    # (taken from antropy package: https://raphaelvallat.com/antropy/
-    x = np.asarray(x)
+def spectral_entropy(psd):
+    """
+    Calculate the spectral entropy of a given power spectrum.
+
+    Spectral entropy measures the complexity or unpredictability of a signal's
+    frequency representation. This function handles zero or negative values in
+    the PSD in a special way:
+    It returns x * log(x) if x is positive, 0 if x == 0, and `np.nan` otherwise
+
+    Inspired by the `antropy` package:
+    https://raphaelvallat.com/antropy/
+
+    Parameters
+    ----------
+    psd : array_like
+        Power spectrum density of a signal. Should be a 1D array or list of
+        non-negative values.
+
+    Returns
+    -------
+    float
+        Spectral entropy of the provided PSD (in bits).
+
+    """
+    x = np.asarray(psd)
     xlogx = np.zeros(x.shape)
     xlogx[x < 0] = np.nan
     valid = x > 0
@@ -32,6 +51,48 @@ def spectral_entropy(x):
 
 
 def frequency_variability(psd, freqs, freq_lim=None):
+    """
+    Compute frequency variability metrics for a given power spectrum.
+
+    This function calculates the aperiodic components (one-over-f exponent and
+    offset) for the power spectrum using the `FOOOF` package
+    (https://fooof-tools.github.io/fooof/index.html). It also computes the
+    spectral entropy of the power spectrum.
+    The calculation is limited to a specified frequency range if
+    `freq_lim` is provided.
+
+    Parameters
+    ----------
+    psd : array_like
+        Power spectrum density (PSD) of a signal. Should be a 1D array or list.
+
+    freqs : array_like
+        List of frequency values corresponding to the PSD.
+
+    freq_lim : tuple, optional
+        Lower and upper frequency bounds as a tuple (low_freq, high_freq).
+        If not provided, the entire frequency range is considered.
+
+    Returns
+    -------
+    dict
+        A dictionary containing:
+        - '1f_offset': one-over-f offset.
+        - '1f_exponent': one-over-f exponent.
+        - 'spectral_entropy': Spectral entropy of the PSD.
+
+    Notes
+    -----
+    `FOOOF` might encounter numerical issues during computation.
+    In such cases, the function will catch exceptions and return `np.nan`
+    for the affected metric.
+
+    Examples
+    --------
+    >>> psd_values = [1, 2, 3, 4, 5]
+    >>> freqs_values = [0.5, 1, 1.5, 2, 2.5]
+    >>> frequency_variability(psd_values, freqs_values)
+    """
 
     if freq_lim is None:
         lwf = freqs[0]
@@ -47,7 +108,7 @@ def frequency_variability(psd, freqs, freq_lim=None):
         [(freq >= lwf) & (freq <= upf) for freq in freqs]
     ]
 
-    # compute 1/f measures
+    # compute one-over-f spectral components
     try:
         with warnings.catch_warnings():
             # just in case weird numerical issues arise
@@ -70,6 +131,44 @@ def frequency_variability(psd, freqs, freq_lim=None):
 
 
 def signal_variability(signal):
+    """
+    Compute signal variability metrics for a signal.
+
+    This function calculates multiple metrics that assess the variability
+    or complexity of a signal, such as permutation entropy, multiscale entropy,
+    and Hjorth parameters.
+
+    Parameters
+    ----------
+    signal : array_like
+        1D array or list containing the signal data points.
+
+    Returns
+    -------
+    dict
+        Dictionary containing various metrics assessing the variability
+        of the signal:
+        - 'permutation_entropy': Permutation entropy of the signal.
+        - 'weighted_permutation_entropy': Weighted permutation entropy.
+        - 'multi-scale entropy': Multiscale entropy values.
+        - 'multi-scale entropy (1-4)': Binned multiscale entropy values.
+        - 'multi-scale entropy (slope)': Slope of the multiscale entropy curve.
+        - 'activity': Variance of the signal scaled by 1e6.
+        - 'mobility': Mobility value from Hjorth parameters.
+        - 'complexity': Complexity value from Hjorth parameters.
+
+    Notes
+    -----
+    Some metrics might encounter numerical issues during computation.
+    In such cases, the function will catch exceptions and return `np.nan`
+    for the affected metric.
+
+     Examples
+    --------
+    >>> signal_data = np.random.rand(1000)
+    >>> variability_metrics = signal_variability(signal_data)
+    >>> variability_metrics['permutation_entropy']
+    """
 
     # permutation entropy
     pent, _ = nk.entropy_permutation(signal,
@@ -131,7 +230,13 @@ def signal_variability(signal):
             'complexity': comp}
 
 
-def process_element(inst, m, n, freqs=None):
+def _process_element(inst, m, n, freqs=None):
+    """
+    This function computes either the frequency variability
+    (if `freqs` is provided) or the signal variability (if `freqs` is
+    not provided) for a specific element located at the coordinates (m, n)
+    of the instance.
+    """
     if freqs is not None:
         return frequency_variability(inst[m, n, :], freqs, freq_lim=[2.0, 45.0])
     else:
@@ -139,6 +244,39 @@ def process_element(inst, m, n, freqs=None):
 
 
 def parallel_analysis(inst, freqs=None, jobs=1):
+    """
+    Run parallel analysis on a given instance. The intance can be an array of
+    amplitude values or power spectral density values for a set of EEG segments.
+
+    The function divides the instance into tasks and processes them in parallel.
+    Each task corresponds to a combination of EEG segments and EEG sensors.
+
+    Parameters
+    ----------
+    inst : array_like, shape (n_epochs, n_channels, n_times or n_frequencies)
+        The time series data consisting of n_epochs for separate observations
+        of signals with n_channels time-series of length n_times.
+
+    freqs : array_like, optional
+        List of frequency values. If provided, `inst` is assumed to be an array
+        of power spectral density values and analysis is computed
+        "in the frequency domain".
+
+    jobs : int, optional
+        Number of CPU cores to use for parallel processing.
+        Default is 1 (no parallelism).
+
+    Returns
+    -------
+    tuple
+        - results: 3D array containing the output from the analysis.
+        - measures: List of measures used in the analysis.
+
+    Examples
+    --------
+    >>> x = np.random.rand(20, 10, 800)
+    >>> results, measures = parallel_analysis(x, jobs=2)  # noqa
+    """
     if freqs is not None:
         results = np.empty((3, inst.shape[0], inst.shape[1]))
     else:
@@ -150,10 +288,12 @@ def parallel_analysis(inst, freqs=None, jobs=1):
     tasks = [(i, j) for i in range(inst.shape[0]) for j in range(inst.shape[1])]
     batch_size = total_tasks // 10
 
-    for i in range(0, len(tasks), batch_size):
-        batch = tasks[i:i + batch_size]
+    measures = None
+    for t in range(0, len(tasks), batch_size):
+        batch = tasks[t:t + batch_size]
         output_list = Parallel(n_jobs=jobs)(
-            delayed(process_element)(inst, i, j, freqs) for (i, j) in batch
+            delayed(_process_element)(inst, i, j, freqs)
+            for (i, j) in batch
         )
 
         for idx, (i, j) in enumerate(batch):
